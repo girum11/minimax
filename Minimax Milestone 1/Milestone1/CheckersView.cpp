2d86cecd63a83b8cf1bd5d7e18ac7eaec6dfc20e
Girum Ibssa <girum@girum.org>
2012-11-29 20:24:47 -0800

    About to do a little CheckersView before I continue stubbing.

--- Minimax Milestone 1/Milestone1/CheckersView.cpp
+++ Minimax Milestone 1/Milestone1/CheckersView.cpp
@@ -1,12 +1,23 @@
+
+#include <assert.h>
 #include "CheckersView.h"
+#include "CheckersBoard.h"
 
 
-CheckersView::CheckersView(void)
-{
-}
+using namespace std;
+
+Class CheckersView::mClass("CheckersView", &CreateCheckersView);
+
+void CheckersView::Draw(ostream &out) {
+   string rtn("");
+   int row = 0, col = 0;
+   char sqr;
+   const CheckersBoard *cb = dynamic_cast<const CheckersBoard *>(mModel);
+   assert(cb != NULL);  // TODO: Yank this.
+
+   
 
 
-CheckersView::~CheckersView(void)
-{
 
+   out << rtn;
 }
