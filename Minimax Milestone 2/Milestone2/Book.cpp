f1d07bf869c03743f1899d8a154c2f03291096c2
Girum Ibssa <girum@girum.org>
2012-12-08 18:47:37 -0800

    More stubs on Book.cpp.  I'm about to ham on it though.

--- Minimax Milestone 2/Milestone2/Book.cpp
+++ Minimax Milestone 2/Milestone2/Book.cpp
@@ -10,7 +10,7 @@ using namespace std;
 // The book file binary format is as follows, with all values BigEndian:
 // 
 // Minimax level at which book was calculated – 1 byte
-// 
+//
 // For each Key/BestMove pair:
 // 
 // Key (BigEndian binary write of all the BasicKey ulongs)
@@ -38,15 +38,48 @@ Book::~Book() {
 
 // [Staley] Read/write book to and from a file in binary format.  
 // [Staley] In Read, assume the Keys and Moves in the file are those for boardClass.
+// [Staley]
+// [Staley] When you read the book, assume that the depth of the BestMove is
+// [Staley] the "level" of the book file.
+// [Staley] Also, assume that the number of boards explored is 0.
 std::istream &Book::Read(std::istream &is, const Class *boardClass) {
-   
-   
+   char temp = '\0';
+   Board *board = dynamic_cast<Board *>(boardClass->NewInstance());
+   Board::Key *key = NULL;
+
+   is.read(&temp, sizeof(mLevel));
+   mLevel = temp;
+
+
+
+   // Don't forget to clean up after you finish
+   delete board;
 
    return is;
 }
 
 // [Staley] Read/write book to and from a file in binary format.
 std::ostream &Book::Write(std::ostream &os) {
+
+   os.write((char *)&mLevel, sizeof(mLevel));
+
+   for (Book::const_iterator bookIter = this->begin();
+    bookIter != this->end(); bookIter++) {
+      
+      // Write the key
+
+      // Write the BestMove
+
+      // TODO: If it exists, write the "replyMove exists" flag.  1 if it exists,
+      // 0 if it does not.
+
+      // TODO: If the aforementioned flag is set, write the replyMove
+
+      // Write the computed minimax value for this BestMove
+
+
+
+   }
 
    
    return os;
