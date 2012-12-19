4c28d4007f87b0903f7458ac02d458b89da0b5db
Girum Ibssa <girum@girum.org>
2012-12-18 18:56:41 -0800

    Style.

--- Minimax Milestone 2/Milestone2/SimpleAIPlayer.cpp
+++ Minimax Milestone 2/Milestone2/SimpleAIPlayer.cpp
@@ -10,26 +10,26 @@ using namespace std;
 // move.  Any 'bestMove->value' V such that V <= min or V >= max is 
 // "uninteresting" to the caller.  
 // 
-// 'minimaxLevel' is always >= 1 and indicates how deeply to probe. (Note that this 
-// means you should evaluate leaf boards directly as they are generated, rather 
-// than making a FindBestMove call with minimaxLevel of 0 for each leaf board.  This is 
-// important for efficient running.)
+// 'minimaxLevel' is always >= 1 and indicates how deeply to probe. (Note that 
+// this means you should evaluate leaf boards directly as they are generated, 
+// rather than making a FindBestMove call with minimaxLevel of 0 for each leaf 
+// board.  This is important for efficient running.)
 // 
 // 'tTable' points to a tTable of previously computed BestMoves, or NULL.
 //
 // 
 // Postconditions: 
 // 'bestMove->value' indicates the result of the minimax search. If 
-// bestMove->value <= min or bestMove->value >= max then no "interesting" move could 
-// be found, and bestMove->move will indicate the first move where this fact 
-// was uncovered.
+// bestMove->value <= min or bestMove->value >= max then no "interesting" move 
+// could be found, and bestMove->move will indicate the first move where this 
+// fact was uncovered.
 // 
 // If 'board' is an endgame board, then  bestMove->value will be winVal, 0 or 
 // -winVal, and bestMove->move will be NULL since there is no move from an 
 // endgame position.  Otherwise, bestMove->move will provide the best move.  
 //
-// 'bestMove->bestReply' gives the best answering move to bestMove->move, or NULL 
-// if bestMove->move ends the game.  
+// 'bestMove->bestReply' gives the best answering move to bestMove->move, or 
+// NULL if bestMove->move ends the game.  
 // 
 // 'bestMove->numBoards' gives the number of boards examined during the minimax 
 // computation, including the root board. 
@@ -44,119 +44,119 @@ using namespace std;
 // during the call.
 
 void SimpleAIPlayer::Minimax(Board *board, int minimaxLevel, long min, long max,
- BestMove *bestMove, Book *tTable, int debugFlag) {
+ BestMove *bMove, Book *tTable, int dbg) {
    list<Board::Move *> moves;
-   list<Board::Move *>::iterator moveIter;
+   list<Board::Move *>::iterator mIter;
    BestMove subBestMove(NULL, NULL, 0, minimaxLevel, 1);
    const Board::Key *key = 0;
-   Book::iterator bookIter;
-   pair<Book::iterator, bool> insRes;
+   Book::iterator bIter;
+   pair<Book::iterator, bool> ins;
 
    // [Staley] Level 0 computations aren’t worth it since a call of GetValue is 
    // [Staley] usually quicker than a tTable lookup.
-   // [Me] So, ensure that MakeBook doesn't call this method with minimaxLevel == 0.
+   // [Me] So, ensure that MakeBook doesn't call this method with 
+   // minimaxLevel == 0.
    assert(minimaxLevel >= 1);
 
    // Before we begin "exploring" this node, first consult the transposition 
    // table to see if we already have a precomputed best move for its
-   // board configuration [Filled blank] "with minimaxLevel at least as deep as the 
-   // one you need."
-   if (tTable && (bookIter = tTable->find(key = board->GetKey())) != tTable->end()
-    && (*bookIter).second.depth >= minimaxLevel) {
+   // board configuration [Filled blank] "with minimaxLevel at least as deep 
+   // as the one you need."
+   if (tTable && (bIter = tTable->find(key = board->GetKey())) != tTable->end()
+    && (*bIter).second.depth >= minimaxLevel) {
       // [Filled blank] If we find the bestMove in the transposition table,
       // then set the bestMove straightaway.
-      *bestMove = (*bookIter).second;
-      bestMove->numBoards = 1;
+      *bMove = (*bIter).second;
+      bMove->numBoards = 1;
    }
    else {
       // To begin "exploring" this node, first figure out what the list of
-      // possible moves is, so that you can construct the nodes at the minimaxLevel
-      // below you (one node created per Move).
+      // possible moves is, so that you can construct the nodes at the 
+      // minimaxLevel below you (one node created per Move).
       board->GetAllMoves(&moves);
 
       // Fill up bestMove -- assume that the bestMove for this node is an empty 
       // BestMove.
-      *bestMove = subBestMove;
+      *bMove = subBestMove;
 
       // Edge case: If this node is an end-game node, then set this bestMove's 
       // value to be the appropriate kWinVal.
       // [Filled blank] Otherwise, bestMove->value should just be the current
       // value of the board.
-      bestMove->value = moves.size() == 0 ? board->GetValue() :
+      bMove->value = moves.size() == 0 ? board->GetValue() :
        (board->GetWhoseMove() ? Board::kWinVal - 1 : -Board::kWinVal + 1);
 
       // Iterate through each of the possible moves, [Filled blank] provided
       // that the limits for this node haven't collided yet.
-      for (moveIter = moves.begin(); min < max && moveIter != moves.end(); 
-       moveIter++) {
+      for (mIter = moves.begin(); min < max && mIter != moves.end(); mIter++) {
 
-         board->ApplyMove(*moveIter);
+         board->ApplyMove(*mIter);
 
          // Base case.  If the minimax recursion can't possibly go down another
          // level because you're at your target Level, then stop recursing down.
          if (minimaxLevel == 1)
             subBestMove.value = board->GetValue();
          else
-            Minimax(board, minimaxLevel-1, min, max, &subBestMove, tTable, debugFlag);
+            Minimax(board, minimaxLevel-1, min, max, &subBestMove, tTable, dbg);
 
          // [Filled blank] Conditional: White pulls the floor up.
          if (board->GetWhoseMove() == 1 && subBestMove.value > min) {
-            bestMove->value = min = subBestMove.value;
+            bMove->value = min = subBestMove.value;
             
             // [Filled blank] Set the best move to be this move.
-            bestMove->SetBestMove((*moveIter)->Clone());
+            bMove->SetBestMove((*mIter)->Clone());
 
             // [Filled blank] Set the reply move to be the subBestMove,
             // and nil out subBestMove's move.
-            bestMove->SetReplyMove(subBestMove.move);
+            bMove->SetReplyMove(subBestMove.move);
             subBestMove.move = NULL;
          }
          // [Filled blank] Conditional: Black pushes the ceiling down.
          else if (board->GetWhoseMove() == 0 && subBestMove.value < max) {
-            bestMove->value = max = subBestMove.value;
+            bMove->value = max = subBestMove.value;
 
             // [Filled blank] Set the reply move to be the subBestMove
-            bestMove->SetBestMove((*moveIter)->Clone());
+            bMove->SetBestMove((*mIter)->Clone());
 
             // [Filled blank] Set the reply move to be the subBestMove,
             // and nil out subBestMove's move.
-            bestMove->SetReplyMove(subBestMove.move);
+            bMove->SetReplyMove(subBestMove.move);
             subBestMove.move = NULL;
          }
 
-         if (debugFlag > 0) {
+         if (dbg > 0) {
             for (int cnt = minimaxLevel-1; cnt > 0; cnt--)
                cout << "   ";
-            cout << "Move " << (string)**moveIter << " nets " << subBestMove.value
+            cout << "Move " << (string)**mIter << " nets " << subBestMove.value
              << " min/max is " << min << "/" << max << endl;
          }
 
          board->UndoLastMove();
-         bestMove->numBoards += subBestMove.numBoards;
+         bMove->numBoards += subBestMove.numBoards;
       }
 
       // [Filled blank] Delete Move pointers contained in any unused nodes.
-      for (; moveIter != moves.end(); moveIter++)
-         delete *moveIter;
+      for (; mIter != moves.end(); mIter++)
+         delete *mIter;
 
       // [Filled blank] From the loop before, a min/max limits collision doesn't
       // return, but breaks instead (to provide time to clean up the 
       // GetAllMoves() call.  Thus, you have to ensure that the tTable isn't
       // added if you had a min/max collision.
-      if (tTable && minimaxLevel >= SAVE_LEVEL && min < max && bestMove->move) {
+      if (tTable && minimaxLevel >= SAVE_LEVEL && min < max && bMove->move) {
          // [Filled blank] Insert the key->bestMove mapping into the map.
-         insRes = tTable->insert(pair<const Board::Key *, BestMove>(key, *bestMove));
+         ins = tTable->insert(pair<const Board::Key *, BestMove>(key, *bMove));
          
          // If you successfully inserted the key, then nil out the pointer to
          // it before you accidentally delete it after this else{} finishes.
-         if (insRes.second)
+         if (ins.second)
             key = 0;
          // [Filled blank] "And, very importantly, we update the table 
          // even if it already has a key for the board you’re computing, if 
          // your new computation is for a deeper lookahead minimaxLevel than 
          // the one in the tTable."
-         else if ((*insRes.first).second.depth < minimaxLevel) {
-            (*insRes.first).second = *bestMove;
+         else if ((*ins.first).second.depth < minimaxLevel) {
+            (*ins.first).second = *bMove;
          } 
       }
    }
